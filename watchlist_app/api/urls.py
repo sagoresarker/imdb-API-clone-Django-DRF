693072c968cd2ecba23a666db3809bab1ed6ddcb
sagoresarker <sagoresarker.cse@gmail.com>
2023-06-23 18:10:24 +0600

    Add different types of class, also concrete class

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -4,10 +4,13 @@ from watchlist_app.api import views
 urlpatterns = [
     path('list/', views.WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>/', views.WatchDetailsAV.as_view() , name='movie-details'),
+    
     path('stream/', views.StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>', views.StreamPlatformDetailsViewAV.as_view(), name='stream-details'),
-
+    path('stream/pythreview/<int:pk>', views.ReviewDetail.as_view(), name='review-details'),
+    
     path('review/', views.ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>', views.ReviewDetail.as_view(), name='review-details'),
+    path('review/<int:pk>', views.ReviewDetail.as_view(), name='review-list'),
+    
 
 ]
