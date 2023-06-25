e21cd384504693d8193e56b5e764fcb1ccda9d41
sagoresarker <sagoresarker.cse@gmail.com>
2023-06-25 14:02:21 +0600

    Implement JWT authentication

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -3,6 +3,7 @@ from user_app.api.serializers import RegistrationSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 @api_view(['POST'])
 def logout_view(request):
@@ -23,8 +24,13 @@ def registration_view(request):
             data['username'] = account.username
             data['email'] = account.email
             
-            token = Token.objects.get(user=account).key
-            data['token'] = token
+            # token = Token.objects.get(user=account).key
+            # data['token'] = token
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                            'refresh': str(refresh),
+                            'access': str(refresh.access_token),
+                        }
         else:
             data = serializer.errors
         
