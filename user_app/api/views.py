76ae958dddb162b20c7d61e784b2752bb5e5e19d
sagoresarker <sagoresarker.cse@gmail.com>
2023-06-25 14:19:46 +0600

    Implemented Global throttling

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -3,7 +3,7 @@ from user_app.api.serializers import RegistrationSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
+#from rest_framework_simplejwt.tokens import RefreshToken
 
 @api_view(['POST'])
 def logout_view(request):
@@ -24,13 +24,13 @@ def registration_view(request):
             data['username'] = account.username
             data['email'] = account.email
             
-            # token = Token.objects.get(user=account).key
-            # data['token'] = token
-            refresh = RefreshToken.for_user(account)
-            data['token'] = {
-                            'refresh': str(refresh),
-                            'access': str(refresh.access_token),
-                        }
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+            # refresh = RefreshToken.for_user(account)
+            # data['token'] = {
+            #                 'refresh': str(refresh),
+            #                 'access': str(refresh.access_token),
+            #             }
         else:
             data = serializer.errors
         
